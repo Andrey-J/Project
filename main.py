d7f8ced9ee89749d10b4581b1fa25ab7586d3426
Stepan-jkr <57662923+Stepan-jkr@users.noreply.github.com>
2020-01-09 14:41:48 +0300

    Add files via upload
    Добавили лазер

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 import os
 import random
 
+pygame.init()
+size = x, y = 1280, 720
+running = True
+count = 0
+screen = pygame.display.set_mode(size)
+screen.fill((255, 255, 255))
+
 
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
@@ -15,16 +22,8 @@ def load_image(name, color_key=None):
     return image
 
 
-pygame.init()
-size = x, y = 800, 600
-count = 0
-running = True
-screen = pygame.display.set_mode(size)
-screen.fill((255, 255, 255))
-
-
 class Space(pygame.sprite.Sprite):
-    image = load_image("space.jpg")
+    image = load_image("space1.jpg")
 
     def __init__(self):
         super().__init__(all_sprites)
@@ -33,22 +32,18 @@ class Space(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.bottom = y
 
-    def background(self):
-        if count == 1000:
-            image = load_image("space1.jpg")
-
 
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 space = Space()
 
 
-class Spacecraft(pygame.sprite.Sprite):
-    image = load_image("spacecraft.jpg", -1)
+class Laser(pygame.sprite.Sprite):
+    image = load_image("laser.png", -1)
 
-    def __init__(self, pos):
+    def __init__(self, pos1):
         super().__init__(all_sprites)
-        self.image = Spacecraft.image
+        self.image = Laser.image
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.x = pos1[0]
@@ -59,27 +54,19 @@ class Spacecraft(pygame.sprite.Sprite):
         if not pygame.sprite.collide_mask(self, space):
             self.rect = self.rect.move(1, 1)
 
-    def sgip(self):
-        if count == 500:
-            image = load_image("spacecraft1.jpg")
-        if count == 1500:
-            image = load_image("spacecraft3.jpg")
-
             
 class Meteor(pygame.sprite.Sprite):
     image = load_image("meteor.jpg", -1)
     
-    def __init__(self, pos):
+    def __init__(self, pos2):
         super().__init__(all_sprites)
         self.image = Meteor.image
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = pos2[]
-        self.rect.y = pos2[]
+        self.rect.x = pos2[499]
+        self.rect.y = pos2[499]
 
-    def asteroid(self):
-        if count == 1500:
-            image = load_image("meteor1.jpg")
+
 
 
 while running:
@@ -87,10 +74,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            Spacecraft(event.pos)
-        if x.pos1[] and y.pos1[] == x.pos2[] and y.pos2[]:
-            count += 1
-            meteor.pop
+            Laser(event.pos)
     screen.fill((0, 0, 0))
     all_sprites.draw(screen)
     all_sprites.update()
